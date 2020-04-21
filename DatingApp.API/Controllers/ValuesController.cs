96ca10a8adfda5d952ada3765bc6efb017a24997
Srinivas Rao <srinivas.raoslr@gmail.com>
2020-04-22 00:46:38 +0530

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public ValuesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {// Returning ActionResult<IEnumerable<string>> will only wrapup type T.
+            //Instead, Better to use IActionResult which will allow to return http response like 200,404, etc.  
+            var result = await _dataContext.Values.ToListAsync();
+            return Ok(result);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var res = await _dataContext.Values.FirstOrDefaultAsync(c => c.Id == id);
+            return Ok(res);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
